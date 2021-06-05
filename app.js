5d26e93a99010e2b81ff903b3a19ad423c3db5b5
M.Tanveer Khan <tanveer.khan2692000@gmail.com>
2021-06-05 19:15:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// DOM 
+// JS 58-67
+
+// 1.
+// var mainContent = document.getElementById('main')
+// var child = mainContent.getElementsByTagName('p')
+// console.log(child)
+// var classRender = document.getElementsByClassName('render')
+// for (var i = 0; i < classRender.length; i++) {
+//     document.write(classRender[i].innerHTML + "<br>")
+// }
+// var inpFirstName = document.getElementById('first-name').innerHTML = 'Muhammad Tanveer'
+// console.log(inpFirstName)
+// var inpLastName = document.getElementById('last-name').innerHTML = 'Khan'
+// console.log(inpLastName)
+// var inpEmail = document.getElementById('email').innerHTML = 'tanveer.khan2692000@gamil.com'
+// console.log(inpEmail)
+
+// 2.
+// var nodeType = document.getElementById('form-content').nodeType
+// document.write(nodeType + "<br>")
+// var idNodeType = document.getElementById('lastName')
+// var last = idNodeType.nodeType
+// document.write(last + "<br>")
+// var lastChildNode = idNodeType.childNodes[0].nodeType
+// document.write(lastChildNode + "<br>")
+// var updateChild = idNodeType.childNodes[0].innerHTML = 'HI'
+// document.write(updateChild)
+// var mainParent = document.getElementById('main')
+// var firstChild = mainParent.firstChild
+// console.log(firstChild)
+// var lastChild = mainParent.lastChild
+// console.log(lastChild)
+// var lastName = document.getElementById('lastName')
+// var nextSib = lastName.nextSibling
+// console.log(nextSib)
+// var preSib = lastName.previousSibling
+// console.log(preSib)
+// var email = document.getElementById('email')
+// console.log(email.nodeType)
+// var parentNode = email.parentNode
+// console.log(parentNode)
+// var parenteNodeType = parentNode.nodeType
+// console.log(parenteNodeType)
